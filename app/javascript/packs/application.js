6e08bdf5d8363786155b2851406107fb02048d1f
北爪康司 <navigator.lp720@gmail.com>
2024-07-29 13:03:32 +0900

    仮ページ作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,12 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.zoomable');
+    images.forEach(img => {
+      img.addEventListener('click', () => {
+        img.classList.toggle('zoomed');
+      });
+    });
+  });
